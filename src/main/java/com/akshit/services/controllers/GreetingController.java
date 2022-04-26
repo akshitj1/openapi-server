d13b7af7291cc951692e9dd1e28522a97905c260
Akshit Jain <akshit.jain@dealshare.in>
2022-04-26 09:47:08 +0530

    object call added

--- src/main/java/com/akshit/services/controllers/GreetingController.java
+++ src/main/java/com/akshit/services/controllers/GreetingController.java
@@ -1,18 +1,31 @@
 package com.akshit.services.controllers;
 
-import io.tej.SwaggerCodgen.api.AliveApi;
+import io.tej.SwaggerCodgen.api.HelloApi;
+import io.tej.SwaggerCodgen.model.Greeter;
+import io.tej.SwaggerCodgen.model.Greetings;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
 
 
 @RestController
-public class GreetingController implements AliveApi {
+public class GreetingController implements HelloApi {
     @Override
     public ResponseEntity<String> isAlive(String name) {
         return ResponseEntity.ok(
                 name.equalsIgnoreCase("akshit")
-                        ?"yes"
-                        :"no");
+                        ? "yes"
+                        : "no");
+    }
+
+    @Override
+    public ResponseEntity<Greetings> receiveGreetings(Greeter greeter) {
+        return ResponseEntity.ok(
+                Greetings.builder()
+                        .reply(MessageFormat.format(
+                                "Yo {0}, how you doing?",
+                                greeter.getName()
+                        )).build());
     }
 }
